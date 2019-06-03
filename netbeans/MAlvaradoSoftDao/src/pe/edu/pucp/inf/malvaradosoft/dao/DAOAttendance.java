f28169d88fe12597719c0d1c59a9991c0718731d
Johanavh06 <Johanavh06@github.com>
2019-06-03 12:03:38 -0500

    v2

---
+++ netbeans/MAlvaradoSoftDao/src/pe/edu/pucp/inf/malvaradosoft/dao/DAOAttendance.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.edu.pucp.inf.malvaradosoft.dao;
+
+import java.util.ArrayList;
+import pe.edu.pucp.inf.MAlvaradoSoft.model.bean.Attendance;
+
+/**
+ *
+ * @author WIN10
+ */
+public interface DAOAttendance {
+    ArrayList <Attendance> queryAllAttendance();
+    int insertAtendance(Attendance attendance);
+    int updateAttendance(Attendance attendance);
+    int deleteAttendance(int id);
+}
---
+++ netbeans/MAlvaradoSoftDao/src/pe/edu/pucp/inf/malvaradosoft/mysql/MySQLAttendance.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.edu.pucp.inf.malvaradosoft.mysql;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import pe.edu.pucp.inf.MAlvaradoSoft.model.bean.Attendance;
+import pe.edu.pucp.inf.malvaradosoft.config.DBManager;
+import pe.edu.pucp.inf.malvaradosoft.dao.DAOAttendance;
+
+/**
+ *
+ * @author WIN10
+ */
+public class MySQLAttendance implements DAOAttendance {
+
+    public ArrayList<Attendance> queryAllAttendance() {
+        ArrayList<Attendance> attendances = new ArrayList<>();
+        try{
+            DBManager dbManager= DBManager.getDbManager();
+            Connection con = DriverManager.getConnection(dbManager.getUrl(), dbManager.getUser(), dbManager.getPassword());
+            String sql = "SELECT * FROM Attendance";
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            
+            while(rs.next()){
+                Attendance a = new Attendance();
+                a.setIdAttendance(rs.getInt("User_idPerson"));
+                a.setDateTime(rs.getDate("Attendance_dateTime"));
+                a.setTableStatus(rs.getInt("Attendance_status"));
+                attendances.add(a);
+            }
+            con.close();
+            
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        return attendances;
+    }
+    
+    @Override
+    public int updateAttendance(Attendance attendance) {
+        int result = 0;
+        try{
+            DBManager dbManager = DBManager.getDbManager();
+            Connection con = DriverManager.getConnection(
+            dbManager.getUrl(), 
+            dbManager.getUser(), 
+            dbManager.getPassword());
+            CallableStatement cs = con.prepareCall(""
+                    + "{call updateAttendance(?,?,?)}");
+            cs.setInt(1, attendance.getIdAttendance());
+            cs.setDate(2, (Date) attendance.getDateTime());
+            cs.setInt(3, attendance.getTableStatus());
+         
+            result = cs.executeUpdate();
+            con.close();
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        return result;
+    }
+
+    @Override
+    public int deleteAttendance(int id) {
+        int result= 0;
+        try{
+            DBManager dbManager= DBManager.getDbManager();
+            Connection con = DriverManager.getConnection(dbManager.getUrl(), dbManager.getUser(), dbManager.getPassword());
+            CallableStatement cs = con.prepareCall(""
+                    + "{call deleteAttedance(?)}");
+			cs.setInt(1, id);
+            result= cs.executeUpdate();
+            con.close();            
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    return result;
+    }
+
+    @Override
+    public int insertAtendance(Attendance attendance) {
+        int result = 0;
+        try{
+            DBManager dbManager = DBManager.getDbManager();
+            Connection con = DriverManager.getConnection(
+            dbManager.getUrl(), 
+            dbManager.getUser(), 
+            dbManager.getPassword());
+            CallableStatement cs = con.prepareCall("{call insertAttendance(?)}");
+            cs.setInt(1, attendance.getIdAttendance());   
+            result = cs.executeUpdate();
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        return result;
+    }
+    
+}
