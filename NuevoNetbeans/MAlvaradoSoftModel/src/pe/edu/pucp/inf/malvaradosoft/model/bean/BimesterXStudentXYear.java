feb7c97916148b42ac3282521ff8a26a9fea49f9
renatorca <a20151207@pucp.pe>
2019-06-16 01:34:46 -0500

    Renato C.

--- NuevoNetbeans/MAlvaradoSoftModel/src/pe/edu/pucp/inf/malvaradosoft/model/bean/BimesterXStudentXYear.java
+++ NuevoNetbeans/MAlvaradoSoftModel/src/pe/edu/pucp/inf/malvaradosoft/model/bean/BimesterXStudentXYear.java
@@ -10,5 +10,41 @@ package pe.edu.pucp.inf.malvaradosoft.model.bean;
  * @author lenovo
  */
 public class BimesterXStudentXYear {
-    
+    private Bimester bimester;
+    private Year year;
+    private Student student;
+
+    public BimesterXStudentXYear() {
+    }
+
+    public BimesterXStudentXYear(Bimester bimester, Year year, Student student) {
+        this.bimester = bimester;
+        this.year = year;
+        this.student = student;
+    }
+
+    public Bimester getBimester() {
+        return bimester;
+    }
+
+    public void setBimester(Bimester bimester) {
+        this.bimester = bimester;
+    }
+
+    public Year getYear() {
+        return year;
+    }
+
+    public void setYear(Year year) {
+        this.year = year;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+            
 }
