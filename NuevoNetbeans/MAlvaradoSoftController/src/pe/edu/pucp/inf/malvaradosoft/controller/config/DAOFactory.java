7707342deebf898109a12c3dc844f2d3988f58a2
Johanavh06 <Johanavh06@github.com>
2019-06-16 16:50:01 -0500

    HOLS

---
+++ NuevoNetbeans/MAlvaradoSoftController/src/pe/edu/pucp/inf/malvaradosoft/controller/config/DAOFactory.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.edu.pucp.inf.malvaradosoft.controller.config;
+
+public abstract class DAOFactory {
+    
+    public static DAOFactory getDAOFactory(){
+        if(DBManager.getdbManager().getUrl().contains("mysql")){
+                return new MySQLDAOFactory();
+        }
+        else{
+                return new MSSQLDAOFactory();
+        }
+            
+    }
+    
+    //public abstract DAOStudent getStudentDAO();
+    
+}
